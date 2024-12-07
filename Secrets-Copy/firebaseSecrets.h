df56ee2f65f0c1ffe7f2be2ecc57940253c193bb
mrbhanuka <mrbhanuka@outlook.com>
2024-12-07 09:32:54 +0530

    version 3.0

--- Secrets-Copy/firebaseSecrets.h
+++
@@ -1,5 +0,0 @@
-//! Firebase credentials
-#define PROJECT_ID "" // Project ID from Firebase
-#define API_KEY ""    // Web API key from Firebase Console
-#define USER_EMAIL "" // Email for Firebase Authentication
-#define USER_PASSWORD ""
